18a5b7d5b0685dcfc8551f380879d448f8d1e37c
Bruno Santos <brunosantos@protonmail.com>
2018-06-07 07:24:00 -0300

    retcon

--- 10_arquivos.py
+++ 10_arquivos.py
@@ -11,29 +11,9 @@ e-mail: belbrunosantos@gmail.com
 """
 
 
-def pasta(lista, tamanho_maximo):
-    """Varre a lista e forma uma pasta."""
-    soma = 0
-    numeros = []
-    for i in range(len(lista)):
-        if i > 0:
-            if soma + lista[i] <= B and len(numeros) < 2:
-                soma = soma + lista[i]
-                lista[i] = 0
-    print(numeros)
-    print(soma)
-
-
 N, B = input().split(' ')
 N = int(N)
 B = int(B)
 arquivos = input().split(' ')
 arquivos = [int(i) for i in arquivos]
 arquivos.sort(reverse=True)
-print(arquivos)
-pasta(arquivos, B)
-print(arquivos)
-pasta(arquivos, B)
-print(arquivos)
-pasta(arquivos, B)
-print(arquivos)
