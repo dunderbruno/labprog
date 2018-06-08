ddd0375be1a8b7810b8de30c462d726ca20ddf29
Bruno Santos <brunosantos@protonmail.com>
2018-06-08 02:40:33 -0300

    formatar saída

--- 09_juvenal.py
+++ 09_juvenal.py
@@ -6,7 +6,6 @@ Juvenal Não Tem o Que Fazer.
 Aluno: Bruno Olimpio dos Santos.
 e-mail: belbrunosantos@gmail.com
 """
-import time
 
 
 def maximo(lista):
@@ -22,33 +21,27 @@ def recursiva(n):
     """Faz chamadas recursivas e registra a quantidade."""
     global rodadas
     if n == 1:
-        # print('1')
         rodadas += 1
     elif (n % 2) == 0:
-        # print('par')
         rodadas += 1
         recursiva(n/2)
     elif (n % 2) == 1:
-        # print('impar')
         rodadas += 1
         recursiva(3*n+1)
     return rodadas
 
-
-rodadas = 0
-# print(recursiva(10))
 
 T = int(input())
 casos = []
 for i in range(T):
     casos.append(input().split(' '))
 
-
-for x in casos:
+rodadas = 0
+for x in range(T):
     maior = 0
-    for y in range(int(x[0]), int(x[1])):
+    for y in range(int(casos[x][0]), int(casos[x][1])):
         atual = recursiva(y)
         if atual > maior:
             maior = atual
         rodadas = 0
-    print(maior)
+    print('Caso %d: %d' % (x+1, maior))
