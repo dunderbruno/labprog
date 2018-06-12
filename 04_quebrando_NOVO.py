bd25e3b42c00318828db25c0a2df79fd97853467
Bruno Santos <brunosantos@protonmail.com>
2018-06-12 10:29:14 -0300

    ajuste no loop da função varredura

--- 04_quebrando_NOVO.py
+++ 04_quebrando_NOVO.py
@@ -7,10 +7,10 @@ saldo = input('Saldo: ')
 # print(len(saldo))
 
 
-si = 0 #inicio do slice
-sf = A - B #fim do slice
+si = 0  # inicio do slice
+sf = A - B  # fim do slice
 rodadas = A - B
-resultado = []
+resultado = ''
 
 
 def varredura(inicio, fim, lista):
@@ -19,22 +19,24 @@ def varredura(inicio, fim, lista):
     for i in range(B+1):
         # print('maior', maior)
         # print(lista[inicio:fim])
-        if int(lista[inicio:fim]) > maior:
+        if len(lista[inicio:fim]) >= 1 and int(lista[inicio:fim]) > maior:
             maior = int(lista[inicio:fim])
             indice = int(inicio)
         inicio += 1
         fim += 1
+    # print('rodei')
     return(indice)
 
 
-print(varredura(si, sf, saldo))
-print(saldo[varredura(si, sf, saldo)])
+# print(varredura(si, sf, saldo))
+# print(saldo[varredura(si, sf, saldo)])
 
 for i in range(rodadas):
-     print(saldo[varredura(si, sf, saldo)])
-     si = int(varredura(si, sf, saldo)) + 1
-     sf -= 1
+    resultado += (saldo[varredura(si, sf, saldo)])
+    si = varredura(si, sf, saldo) + 1
+    sf = sf + 1
 
+print(resultado)
 # print(saldo[(varredura(inicio, fim, saldo))])
 # print(saldo[partida+1:])
 # print(varredura(2, fim, saldo))
