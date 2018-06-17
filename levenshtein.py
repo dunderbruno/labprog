007cb9c5206560501bed4d797bc3470dad2e2ad8
Bruno Santos <brunosantos@protonmail.com>
2018-06-17 18:33:58 -0300

    levenshtein - transformando em função

--- levenshtein.py
+++ levenshtein.py
@@ -1,34 +1,34 @@
 """Levenshtein Distance."""
 
-A = input()
-B = input()
-m = len(A)
-n = len(B)
 
-M = []
+def levenshtein(A, B):
+    """Levenshtein Distance."""
+    m = len(A)
+    n = len(B)
+    M = []
 
-for x in range(m+1):
-    M.append([])
-    for y in range(n+1):
-        M[x].append((0))
+    for x in range(m+1):
+        M.append([])
+        for y in range(n+1):
+            M[x].append((0))
 
-for i in range(1, m+1):
-    M[i][0] = i-1
+    for i in range(1, m+1):
+        M[i][0] = i-1
 
-for j in range(1, n+1):
-    M[0][j] = j-1
+    for j in range(1, n+1):
+        M[0][j] = j-1
 
-for x in range(1, m+1):
-    for y in range(1, n+1):
-        if A[x-1] == B[y-1]:
-            cost = 0
-        else:
-            cost = 2
+    for x in range(1, m+1):
+        for y in range(1, n+1):
+            if A[x-1] == B[y-1]:
+                cost = 0
+            else:
+                cost = 2
 
-        M[x][y] = min(
-                      M[x-1][y-1] + cost,
-                      M[x-1][y] + 1,
-                      M[x][y-1] + 1,
-                      )
+            M[x][y] = min(
+                          M[x-1][y-1] + cost,
+                          M[x-1][y] + 1,
+                          M[x][y-1] + 1,
+                          )
 
-print(M[m][n])
+    return(M[m][n])
