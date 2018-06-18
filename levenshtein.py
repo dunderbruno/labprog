922a3c461cde067a4ab925de6a6616c6f5a95c1a
Bruno Santos <brunosantos@protonmail.com>
2018-06-18 15:20:09 -0300

    ajustes levenshtein

--- levenshtein.py
+++ levenshtein.py
@@ -12,21 +12,21 @@ def levenshtein(A, B):
         for y in range(n+1):
             M[x].append((0))
 
-    for i in range(1, m+1):
-        M[i][0] = i-1
+    for i in range(0, m+1):
+        M[i][0] = i
 
-    for j in range(1, n+1):
-        M[0][j] = j-1
+    for j in range(0, n+1):
+        M[0][j] = j
 
     for x in range(1, m+1):
         for y in range(1, n+1):
             if A[x-1] == B[y-1]:
-                cost = 0
+                C = 0
             else:
-                cost = 2
+                C = 2
 
             M[x][y] = min(
-                          M[x-1][y-1] + cost,
+                          M[x-1][y-1] + C,
                           M[x-1][y] + 1,
                           M[x][y-1] + 1,
                           )
