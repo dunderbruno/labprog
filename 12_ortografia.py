b6a080fb14cacfcd38521a1f2be1d90da3dd8603
Bruno Santos <brunosantos@protonmail.com>
2018-06-19 08:22:13 -0300

    main loop 12_ortografia.py

--- 12_ortografia.py
+++ 12_ortografia.py
@@ -12,3 +12,16 @@ N, M = int(N), int(M)
 
 dicionario = [input() for i in range(N)]
 formulario = [input() for i in range(M)]
+
+
+def refere(palavra):
+    """Retorna palavras do dicionario com Levenshtein <= 2."""
+    saida = ''
+    for i in dicionario:
+        if levenshtein(palavra, i) <= 2:
+            saida = saida + i + ' '
+    return(saida)
+
+
+for i in formulario:
+    print(refere(i)[:-1])
