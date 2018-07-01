aa302f7d764426dd1ba5ffd5bcf92872b7137ba8
Bruno Santos <brunosantos@protonmail.com>
2018-07-01 11:16:43 -0300

    bolo de rolo - versão árvore binária

---
+++ 05_boloderolo_B.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+u"""
+Bolo de Rolo.
+
+Versão com Árvore Binária
+"""
+
+from labtools import maximo
+from labtools import minimo
+from binarytree import *
+
+
+def fatias(tamanhos, largura):
+    """Em quantas fatias iguais um conjunto pode ser dividido."""
+    fatias = 0
+    for i in tamanhos:
+        fatias = fatias + (i//largura)
+    return fatias
+
+
+N = int(input())
+K = int(input())
+bolos = input().split(' ')
+bolos = [int(i) for i in bolos]
+
+menor = minimo(bolos)
+maior = maximo(bolos)
+
+x = tree(None)
+x.populate(1, maior)
+atual = x.raiz
+while True:
+    if fatias(bolos, atual.getChave()) == N:
+        print(atual.getChave())
+        break
+    elif fatias(bolos, atual.getChave()) > N:
+        atual = atual.getDir()
+    elif fatias(bolos, atual.getChave()) < N:
+        atual = atual.getEsq()
