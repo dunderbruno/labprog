393ba20f86c478cbcfc6118bf4ed4e6e03d531af
Bruno Santos <brunosantos@protonmail.com>
2018-07-03 15:16:14 -0300

    15 main

--- 15_banco.py
+++ 15_banco.py
@@ -3,3 +3,24 @@ Juvenal Vai ao Banco Brigar.
 
 Aluno: Bruno Olimpio dos Santos
 """
+
+from lista_encadeada import node
+from lista_encadeada import Lista
+
+T = int(input('T: '))  # casos de teste
+for t in range(T):
+    A = Lista()  # fila regular
+    B = Lista()  # fila preferencial
+    N = int(input('N: '))  # quantidade de comandos em cada caso
+    for n in range(N):
+        comando = input('~$ ')
+        if comando[0] == 'f':
+            A.insertAtEnd(comando.split(' ')[1])
+        elif comando[0] == 'p':
+            B.insertAtEnd(comando.split(' ')[1])
+        elif comando == 'A':
+            A.removeFromBegin()
+        elif comando == 'B':
+            B.removeFromBegin()
+        elif comando == 'I':
+            print(A.primeiro.getDado(), ' ', B.primeiro.getDado())
