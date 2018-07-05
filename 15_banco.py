772dd7f9121cc4927121e4c4c26ecc13f703ec60
Bruno Santos <brunosantos@protonmail.com>
2018-07-05 19:34:41 -0300

    faz a troca de filas caso uma delas esteja vazia

--- 15_banco.py
+++ 15_banco.py
@@ -7,8 +7,12 @@ Aluno: Bruno Olimpio dos Santos
 from lista_encadeada import node
 from lista_encadeada import Lista
 
+
+saidas = []
+
 T = int(input('T: '))  # casos de teste
 for t in range(T):
+    saidas.append('Caso %s:' % str(t+1))
     A = Lista()  # fila regular
     B = Lista()  # fila preferencial
     N = int(input('N: '))  # quantidade de comandos em cada caso
@@ -19,8 +23,39 @@ for t in range(T):
         elif comando[0] == 'p':
             B.insertAtEnd(comando.split(' ')[1])
         elif comando == 'A':
-            A.removeFromBegin()
+            if A.isEmpty():
+                if B.isEmpty():
+                    pass
+                B.removeFromBegin()
+            else:
+                A.removeFromBegin()
         elif comando == 'B':
-            B.removeFromBegin()
+            if B.isEmpty():
+                if A.isEmpty():
+                    pass
+                A.removeFromBegin()
+            else:
+                B.removeFromBegin()
         elif comando == 'I':
-            print(A.primeiro.getDado(), ' ', B.primeiro.getDado())
+            if A.isEmpty():
+                a = '0'
+            else:
+                a = A.primeiro.getDado()
+            if B.isEmpty():
+                b = '0'
+            else:
+                b = B.primeiro.getDado()
+            print('%s %s' % (str(a), str(b)))
+            # saidas.append('%s %s' % (str(a), str(b)))
+            # print(A.primeiro.getDado(), ' ', B.primeiro.getDado())
+
+for i in saidas:
+    print(i)
+# 3
+# 6
+# f 3
+# I
+# p 5
+# p 7
+# A
+# I
