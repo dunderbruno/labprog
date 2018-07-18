5934b529d7c18b9c1926fa518efa5b8842a93603
Bruno Santos <brunosantos@protonmail.com>
2018-07-18 20:54:11 -0300

    add QuickSort

--- labtools.py
+++ labtools.py
@@ -45,6 +45,22 @@ def reverse(lista):
             i -= 1
         lista[i+1] = chave
 
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, (r-1)+1):
+        if A[j] < x:  # para ordem decrescente inverter operador < para >
+            i = i + 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return i+1
+
+
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q-1)
+        quicksort(A, q+1, r)
 
 def levenshtein(A, B):
     """Levenshtein Distance."""
