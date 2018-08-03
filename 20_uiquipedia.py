d854d0c7c870f2be2fb0108e26c8d4184a444c59
Bruno Santos <brunosantos@protonmail.com>
2018-08-03 01:12:22 -0300

    add edge values

--- 20_uiquipedia.py
+++ 20_uiquipedia.py
@@ -42,8 +42,27 @@ class vertex():
         self.edges[vertex.id] = weight
 
 
+def dijkstra(graph, source, destino):
+    """Dijkstra's Algorithm."""
+    Q = []
+    for v in graph:
+        v.distance = math.inf
+        Q.append(v)
+    source.distance = 0
+    while Q:
+        v = min(Q)  # ítem de Q com menor self.distance
+        Q.remove(v)
+        for u in v.neighbors:  # para cada vizinho u de v faça:
+            new = v.distance + v.edges[u.id]
+            if new < u.distance:  # u é o vizinho de v. Na 1 rodada é infinito
+                u.distance = new
+    return destino.distance
+
+
 N = int(input())
 diretas = [input().split(" ") for i in range(N)]
+void = input()
+origem, destino = input().split(" ")
 
 alfabetica = []
 for i in diretas:
@@ -58,13 +77,19 @@ for a in alfabetica:
 
 for i in range(1, len(alfabetica)):
     referencias[alfabetica[i]].addNeighbor(referencias[alfabetica[i-1]])
+    referencias[alfabetica[i]].addEdge(referencias[alfabetica[i-1]], 1)
 
 for i in range(len(alfabetica)-1):
     referencias[alfabetica[i]].addNeighbor(referencias[alfabetica[i+1]])
+    referencias[alfabetica[i]].addEdge(referencias[alfabetica[i+1]], 1)
 
 for i in diretas:
     referencias[i[0]].addNeighbor(referencias[i[1]])
+    referencias[i[0]].addEdge(referencias[i[1]], 1)
     referencias[i[1]].addNeighbor(referencias[i[0]])
+    referencias[i[1]].addEdge(referencias[i[0]], 1)
 
-for r in referencias.values():
-    print(r, r.neighbors)
+# for r in referencias.values():
+#     print(r, r.neighbors)
+
+print(dijkstra(referencias.values(), referencias[origem], referencias[destino]))
