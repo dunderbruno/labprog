8ef032b5620ddd05d2335319a34c132810a7a050
Bruno Santos <brunosantos@protonmail.com>
2019-07-14 09:45:59 -0300

    testes para máximo e mínimo

--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import unittest
 
 from labtools import fatorial
+from labtools import maximo
+from labtools import minimo
 
 
 class TestFatorial(unittest.TestCase):
@@ -10,6 +12,26 @@ class TestFatorial(unittest.TestCase):
         """
         result = fatorial(5)
         self.assertEqual(result, 120)
+
+
+class TestMaximo(unittest.TestCase):
+    def test_int_list(self):
+        """
+        Testa a saída para a entrada de uma lista de inteiros
+        """
+        lista = [1, 2, 3]
+        result = maximo(lista)
+        self.assertEqual(result, 3)
+
+
+class TestMinimo(unittest.TestCase):
+    def test_int_list(self):
+        """
+        Testa a saída para a entrada de uma lista de inteiros
+        """
+        lista = [1, 2, 3]
+        result = minimo(lista)
+        self.assertEqual(result, 1)
 
 
 if __name__ == '__main__':
